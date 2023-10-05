a066f84cfccc8220ccea5aa8c538c6c4a71b599b
Stefan Seepaul <stef1693@gmail.com>
2023-10-05 16:20:11 -0400

    Update streamlit_app.py
    Added Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit
+import pandas
 
 streamlit.title ('My Parents New Healthy Dinner')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinich & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.text ( 🥣 'Omega 3 & Blueberry Oatmeal')
+streamlit.text ( 🥗 'Kale, Spinich & Rocket Smoothie')
+streamlit.text ( 🐔 'Hard-Boiled Free-Range Egg')
+streamlit.text ( 🥑🍞 'Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe (my_fruit_list)
